e1e702cc6fecb081eef4a737420c8c1b42c8ea27
Diogo de Andrade Miranda <dandrade@img.com.br>
2020-06-07 18:07:44 -0300

    feat(server):add validation with celebrate

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { errors } from "celebrate";
 
 import routes from "./routes";
 
@@ -12,5 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.resolve(__dirname, "..", "uploads")));
 app.use('/assets', express.static(path.resolve(__dirname, "..", "assets")));
+
+app.use(errors());
 
 app.listen(3333, () => console.log("Server running"));
--- server/src/routes/points.route.ts
+++ server/src/routes/points.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 import multerConfig from '../config/multer';
 
 import PointsController from "../controllers/points.controller";
@@ -9,14 +10,25 @@ const pointsController = new PointsController();
 
 const upload = multer(multerConfig);
 
+const validation = {
+    body: Joi.object().keys({
+        name: Joi.string().required(),  
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(), //TODO regex
+    })
+}
+
 routes
     .get("/points", pointsController.index)
     .get("/points/:id", pointsController.show)
-    .post("/points", upload.single('image'), pointsController.create)
+    .post("/points",
+        upload.single('image'),
+        celebrate(validation, { abortEarly: false }),
+        pointsController.create)
 
 export default routes;
-
-
-// TODO procurar por
-// serializacao
-// api transform
